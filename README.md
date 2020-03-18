89b1c65ad399d537bfbe73a936cf123373b22081
iyuuya <i.yuuya@gmail.com>
2020-03-19 01:14:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# go-typing
+
+Typing game written in Golang
+
+TRY: タイピングゲームを作ろう - 9. ゴールーチンとチャネル
+(https://tech.mercari.com/entry/2020/03/17/120137)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var words []string
+
+func setup() {
+	words = []string{
+		"hello",
+		"world",
+		"golang",
+		"root",
+		"insert",
+		"update",
+		"vim",
+		"emacs",
+		"word",
+		"string",
+	}
+}
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- string(s.Bytes())
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func getRandomWord() string {
+	t := time.Now().UnixNano()
+	rand.Seed(t)
+	i := rand.Intn(len(words))
+	return words[i]
+}
+
+type result struct {
+	ok int
+	ng int
+}
+
+func (r *result) record(res bool) {
+	if res {
+		r.ok++
+	} else {
+		r.ng++
+	}
+}
+
+func mainLoop(ctx context.Context) {
+	res := result{}
+
+	ch := input(os.Stdin)
+
+	go func() {
+		for {
+			word := getRandomWord()
+			fmt.Printf("==> %s\n", word)
+			fmt.Print(">")
+			inputWord := <-ch
+			res.record(inputWord == word)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("OK: %d, NG: %d\n", res.ok, res.ng)
+	}
+}
+
+func main() {
+	setup()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	mainLoop(ctx)
+
+}
